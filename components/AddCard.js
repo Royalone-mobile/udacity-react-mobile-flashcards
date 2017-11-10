3660302b4e770f74094acd3ec6c79e7a6d70778b
Michael Eisenbraun <meisenbraun@eureka.local>
2017-11-10 11:02:28 -0500

    refactor: Renamed AddQuestion to AddCard.

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 
-class AddQuestion extends Component {
+class AddCard extends Component {
   render() {
     return (
       <View>
-        <Text>Add Question</Text>
+        <Text>Add Card</Text>
       </View>
     )
   }
 }
 
-export default AddQuestion
+export default AddCard
