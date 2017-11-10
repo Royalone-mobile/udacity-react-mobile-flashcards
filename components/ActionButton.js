dbedccf5efb1ccac8f2bb16888d88f23cc03d7a0
Michael Eisenbraun <meisenbraun@eureka.local>
2017-11-10 07:52:46 -0500

    feat: Create a Action Button stateless component

---
+++ components/ActionButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { purple, white } from '../utils/colors'
+
+export default function ActionButton ({ children, onPress, style = {} }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{children}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: purple,
+    padding: 10,
+    margin: 10
+  },
+  buttonText: {
+    color: white,
+    fontSize: 18,
+    textAlign: 'center'
+  }
+})
