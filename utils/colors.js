55d7f3730e3eb81bbc695bdd90736ddd4b32b6ac
Michael Eisenbraun <meisenbraun@eureka.local>
2017-11-12 21:51:44 -0500

    feat: Adds new colors

--- utils/colors.js
+++ utils/colors.js
@@ -1,10 +1,13 @@
 // utils/colors.js
 
 export const purple = '#292477'
-export const gray = '#757575'
+export const gray = '#eaeaea'
+export const darkGray = '#999'
 export const white = '#fff'
 export const red = '#b71845'
 export const orange = '#f26f28'
 export const blue = '#4e4cb8'
 export const lightPurp = '#7c53c3'
 export const pink = '#b93fb3'
+export const black = '#333'
+export const darkBlue = '#3D4A6A'
