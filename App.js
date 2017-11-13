7f50563c0b01b851513387be9ace253efec24ecc
Michael Eisenbraun <meisenbraun@eureka.local>
2017-11-12 21:59:32 -0500

    feat: design changes to deck list view
    New layout and design.
    Moves to list like view
    Removes Tab navigation
    Users AlertIos to add a new deck
    Adds swipe right to show deck delete button

--- App.js
+++ App.js
@@ -8,63 +8,63 @@ import AddDeck from './components/AddDeck'
 import Deck from './components/Deck'
 import AddCard from './components/AddCard'
 import Quiz from './components/Quiz'
-import { white, purple } from './utils/colors'
+import { white, purple, black, darkBlue} from './utils/colors'
 import { setLocalNotification } from './utils/helpers'
 
 function FlashcardsStatusBar ({backgroundColor, ...props}) {
   return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+    <View style={{ height: Constants.statusBarHeight, backgroundColor }}>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
   )
 }
 
 
-const Tabs = TabNavigator({
-  Decks: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'Decks',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    },
-  },
-  AddDeck: {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarLabel: 'New Deck',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    },
-  }
-}, {
-  navigationOptions: {
-    headerTintColor: white,
-    headerStyle: {
-      backgroundColor: 'purple',
-    }
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? purple : white,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? white : purple,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
+// const Tabs = TabNavigator({
+//   Decks: {
+//     screen: DeckList,
+//     navigationOptions: {
+//       tabBarLabel: 'Decks',
+//       tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+//     },
+//   },
+//   AddDeck: {
+//     screen: AddDeck,
+//     navigationOptions: {
+//       tabBarLabel: 'New Deck',
+//       tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+//     },
+//   }
+// }, {
+//   navigationOptions: {
+//     headerTintColor: 'black',
+//     headerStyle: {
+//       backgroundColor: 'white',
+//     }
+//   },
+//   tabBarOptions: {
+//     activeTintColor: Platform.OS === 'ios' ? purple : white,
+//     style: {
+//       height: 56,
+//       backgroundColor: Platform.OS === 'ios' ? white : purple,
+//       shadowColor: 'rgba(0, 0, 0, 0.24)',
+//       shadowOffset: {
+//         width: 0,
+//         height: 3
+//       },
+//       shadowRadius: 6,
+//       shadowOpacity: 1
+//     }
+//   }
+// })
 
 const MainNavigator = StackNavigator({
   Home: {
-    screen: Tabs,
+    screen: DeckList,
     navigationOptions: {
       headerTintColor: white,
       headerStyle: {
-        backgroundColor: purple,
+        backgroundColor: darkBlue,
       }
     }
   },
@@ -73,7 +73,7 @@ const MainNavigator = StackNavigator({
     navigationOptions: {
       headerTintColor: white,
       headerStyle: {
-        backgroundColor: purple,
+        backgroundColor: darkBlue,
       }
     }
   },
@@ -82,7 +82,7 @@ const MainNavigator = StackNavigator({
     navigationOptions: {
       headerTintColor: white,
       headerStyle: {
-        backgroundColor: purple,
+        backgroundColor: darkBlue,
       }
     }
   },
@@ -91,7 +91,7 @@ const MainNavigator = StackNavigator({
     navigationOptions: {
       headerTintColor: white,
       headerStyle: {
-        backgroundColor: purple,
+        backgroundColor: darkBlue,
       }
     }
   }
@@ -105,7 +105,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <FlashcardsStatusBar backgroundColor={purple} barStyle="light-content" />
+        <FlashcardsStatusBar backgroundColor={darkBlue} barStyle="light-content" />
         <MainNavigator />
       </View>
     );
@@ -115,7 +115,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eaeaea',
     alignItems: 'center',
     justifyContent: 'center',
   },
