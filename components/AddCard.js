5ca31e54bcd800a027d5d6da076afdb282864159
Michael Eisenbraun <meisenbraun@eureka.local>
2017-11-13 18:46:17 -0500

    Refactor: removes unused imports.

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Platform, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import { white, gray, darkGray, textGray } from '../utils/colors'
 import ActionButton from './ActionButton'
 import DisabledButton from './DisabledButton'
